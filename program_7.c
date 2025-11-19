dcb43ffdae2e86c9032911e47ed7d87735c908a4
Yashika Rathore <yashikarathore4321@gmail.com>
2025-11-19 17:39:08 +0530

    Added Practical 7

---
+++ program_7.c
@@ -0,0 +1,99 @@
+//Name-Yashika Rathore Roll no.- CT-A-26
+#include <stdio.h>
+#define MAX 20
+int isopd(char c)
+{
+    if((c>='a' && c<='z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
+        return 1;
+    else
+        return 0;
+}
+int isopr(char c)
+{
+    if(c == '+'|| c=='-'|| c =='*'|| c=='/'|| c=='^')
+        return 1;
+    else
+        return 0;
+}
+int priority(char ch)
+{
+    if(ch == '^')
+        return 3;
+    if(ch =='*' || ch == '/')
+        return 2;
+    else
+        return 1;
+}
+int main()
+{
+char infix[MAX], stk[MAX], opd[MAX];
+int i=0, j=0, top=-1;
+char ch;
+printf("\nEnter the infix expression: ");
+scanf("%s",infix);
+while(infix[i]!='\0')
+{
+    ch = infix[i];
+    printf("\n char at position %d is %c", i,ch);
+    if(ch == '(')
+    {
+        top++;
+        stk[top]=ch;
+        //printf("\n\t\t stk[top] = %c", stk[top]);
+    }
+    else if(isopd(ch))
+    {
+        opd[j] = ch;
+        //printf("\n\t\t\t opd[j] = %c", opd[j]);
+        j++;
+
+    }
+    else if(isopr(ch))
+    {
+        if(stk[top] =='(' || priority(ch)>priority(stk[top]))
+        {
+            top++;
+            stk[top]= ch;
+            //printf("\n\t\t stk[top] = %c", stk[top]);
+        }
+        else
+        {
+            while(isopr(stk[top])&& priority(stk[top])>=priority(ch))
+            {
+                opd[j] = stk[top];
+                top--;
+                //printf("\n\t\t\t opd[j] = %c", opd[j]);
+                j++;
+            }
+            top++;
+            stk[top] = ch;
+        }
+
+    }
+    /*else if(ch =='(')
+    {
+        top++;
+        stk[top] = ch;
+        //printf("\n\t\t stk[top] = %c", stk[top]);
+    }*/
+    else
+    {
+        while(stk[top]!= '(')
+        {
+            opd[j] = stk[top];
+            //printf("\n\t\t\t opd[j] = %c", opd[j]);
+            j++;
+            top--;
+        }
+            top--;
+    }
+i++;
+}
+while(top != -1)
+{
+    opd[j++] = stk[top--];
+}
+opd[j] = '\0';
+printf("\n\t\t Postfix expression is %s", opd);
+return 0;
+}
